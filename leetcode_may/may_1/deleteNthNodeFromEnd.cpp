dc6700326afd98850d99c264d9036d7b5c3f075f
mincTeja <himateja135@gmail.com>
2020-05-06 08:16:25 +0530

    delete nth node from end

---
+++ leetcode_may/may_1/deleteNthNodeFromEnd.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *first=head,*second=head;
+        for(int i=0;i<n;++i)
+            second=second->next;
+        if(second==NULL)
+            return head->next;
+        while(second->next!=NULL)
+        {
+            second=second->next;
+            first=first->next;
+        }
+        ListNode *tmp=first->next;
+        first->next=first->next->next;
+        delete tmp;
+        return head;
+    }
+};
